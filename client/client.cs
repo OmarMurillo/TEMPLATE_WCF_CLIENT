dfd825c3ff09eab8aa9a556cb561198bd9043563
OmarMurillo <omar.murillo@inteligene.com>
2024-09-10 12:51:24 -0600

    feat: Initial commit

---
+++ client/client.cs
@@ -0,0 +1,95 @@
+﻿//----------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//----------------------------------------------------------------
+
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Microsoft.Samples.MessageInterceptor
+{
+    [ServiceContract]
+    public interface ISampleContract
+    {
+        [OperationContract(IsOneWay = true)]
+        void ReportWindSpeed(XElement speed);
+    }
+
+    public class MessageModifier : ChannelMessageInterceptor
+    {
+        int send = 0;
+
+        public override void OnSend(ref Message msg)
+        {
+            Console.WriteLine("Hola");
+          
+            Console.WriteLine("Urn interceptor");
+            send++;
+            if (send % 2 == 0)
+            {
+                // Add extra header so that when the message arrives on service side, the message won't be dropped
+              
+                msg.Headers.Add(MessageHeader.CreateHeader("ByPass", "urn:InterceptorNamespace", "ByPassPassword"));
+       
+            }
+            msg.Headers.Add(MessageHeader.CreateHeader("ByPass", "urn:InterceptorNamespace", "ByPassPassword"));
+            msg.Headers.To = new Uri("urn:Interceptor");
+            Console.WriteLine(msg.Headers.To);
+        }
+
+        public override ChannelMessageInterceptor Clone()
+        {
+            return new MessageModifier();
+        }
+    }
+
+    class MessageModifierElement : InterceptingElement
+    {
+        protected override ChannelMessageInterceptor CreateMessageInterceptor()
+        {
+            return new MessageModifier();
+        }
+    }
+
+    class MessageModifierImporter : InterceptingBindingElementImporter
+    {
+        protected override ChannelMessageInterceptor CreateMessageInterceptor()
+        {
+            return new MessageModifier();
+        }
+    }
+
+    class Client
+    {
+        static void Main(string[] args)
+        {
+            ChannelFactory<ISampleContract> channelFactory = new ChannelFactory<ISampleContract>("sampleProxy");
+            ISampleContract proxy = channelFactory.CreateChannel();
+
+            int[] windSpeeds = new int[] { 100, 90, 80, 70, 60, 50, 40, 30, 20, 10 };
+
+            Console.WriteLine("Reporting the next 10 wind speeds.");
+            for (int i = 0; i < 1; i++)
+            {
+                Console.WriteLine(windSpeeds[i] + " kph");
+                try
+                {
+                    XElement element = new XElement("orders", 100);
+                    proxy.ReportWindSpeed(element);
+                }
+                catch (CommunicationException)
+                {
+                    Console.WriteLine("Server dropped a message.");
+                }
+            }
+
+            Console.WriteLine("Press ENTER to shut down client");
+            Console.ReadLine();
+
+            ((IChannel)proxy).Close();
+            channelFactory.Close();
+        }
+    }
+}
---
+++ service/service.cs
@@ -0,0 +1,99 @@
+﻿//----------------------------------------------------------------
+// Copyright (c) Microsoft Corporation.  All rights reserved.
+//----------------------------------------------------------------
+
+using System;
+using System.ServiceModel;
+using System.ServiceModel.Channels;
+using System.Xml;
+
+namespace Microsoft.Samples.MessageInterceptor
+{
+    // Create a service contract and define the service operations.
+    [ServiceContract]
+    public interface ISampleContract
+    {
+        [OperationContract(IsOneWay = true, Action = "http://company_fake/http_endpoint")]
+        void ReportWindSpeed(XmlElement speed);
+    }
+
+    // The Service implementation implements your service contract.
+    public class SampleService : ISampleContract
+    {
+        public void ReportWindSpeed(XmlElement speed)
+        {
+            Console.WriteLine(speed + " --------------------- ");
+           
+        }
+    }
+
+    class DroppingServerElement : InterceptingElement
+    {
+        protected override ChannelMessageInterceptor CreateMessageInterceptor()
+        {
+            return new DroppingServerInterceptor();
+        }
+    }
+
+    public class DroppingServerInterceptor : ChannelMessageInterceptor
+    {
+        int messagesSinceLastReport = 0;
+        readonly int reportPeriod = 5;
+
+        public DroppingServerInterceptor() { }
+
+        public override void OnReceive(ref Message msg)
+        {
+
+            msg.Headers.To = new Uri("urn:SAP:");
+            //Console.WriteLine(msg.ToString());  
+            Console.WriteLine(msg.Headers.To);
+          
+            if(msg.Headers.Action == "http://tempuri.org/ISampleContract/ReportWindSpeed")
+            {
+                msg.Headers.Action = "http://company_fake/http_endpoint";
+            }
+            msg.Headers.To = new Uri("http://localhost:8000/windspeed/");
+            Console.WriteLine(msg.Headers.Action);
+            return;
+            if (msg.Headers.To.ToString() == "urn:SAP:")
+            {
+                Console.WriteLine(reportPeriod + " x2 wind speed reports have been received.");
+                return;
+            }
+           
+            // Drop incoming Message if the Message does not have the special header
+            msg = null;
+        }
+
+        public override ChannelMessageInterceptor Clone()
+        {
+            return new DroppingServerInterceptor();
+        }
+    }
+
+    class Service
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost serviceHost = new ServiceHost(typeof(SampleService), new Uri("http://localhost:8000/windspeed/"));
+            bool success = false;
+
+            try
+            {
+                serviceHost.Open();
+
+                System.Console.WriteLine("Press ENTER to exit.");
+                System.Console.ReadLine();
+
+                serviceHost.Close();
+                success = true;
+            }
+            finally
+            {
+                if (!success)
+                    serviceHost.Abort();
+            }
+        }
+    }
+}
